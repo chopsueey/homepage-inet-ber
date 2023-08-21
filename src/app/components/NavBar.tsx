40545dd3455ccaab895584f197d810d59797380a
chopsueey <meyer-marius@live.de>
2023-08-21 13:39:31 +0200

    init

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return  (
+        <nav className="flex justify-around">
+            <ul className="flex space-x-2">
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/app/components/Section.tsx
@@ -0,0 +1,15 @@
+export default function Section() {
+    return (
+        <section>
+            <h1>Sections for homepage: </h1>
+            <ul>
+                <li>Webdevelopment</li>
+                <li>Gameserver deployment</li>
+                <li>Discord community</li>
+                <li>Custom Bots</li>
+                <li>Streaming zubehör</li>
+                <li>Gaming hardware</li>
+            </ul>
+        </section>
+    )
+}
